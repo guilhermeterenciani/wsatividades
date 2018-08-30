8f30a0fb1d85db57e530d6d619de6731ea200ce0
ternciani <guilhermeterenciani@gmail.com>
2018-08-29 21:52:43 -0400

    Projeto inicial com spring e rest

---
+++ src/main/java/br/edu/ifms/atividades/WsatividadesApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifms.atividades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsatividadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsatividadesApplication.class, args);
+	}
+}
