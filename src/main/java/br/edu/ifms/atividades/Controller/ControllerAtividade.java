7343f558b79109ec1947b3a95666adf9fa5926eb
ternciani <guilhermeterenciani@gmail.com>
2018-09-15 15:44:46 -0400

    Post Funcionando com o Curl

--- src/main/java/br/edu/ifms/atividades/Controller/ControllerAtividade.java
+++ src/main/java/br/edu/ifms/atividades/Controller/ControllerAtividade.java
@@ -2,6 +2,8 @@ package br.edu.ifms.atividades.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,11 @@ public class ControllerAtividade {
 	public List<Atividade> buscarTodasAtividades(){
 		return repositoryAtividade.findAll();
 	}
+	
+	@PostMapping("/atividades")
+	public Atividade novaAtividade(@RequestBody Atividade novaAtividade) {
+		return repositoryAtividade.save(novaAtividade);
+	}
+	
 	
 }
